7918391b335629d01504ebaae6ada393cb34f7ea
himanshuwestcliff <himanshu.dahal@westcliff.edu>
2025-03-29 02:47:04 -0700

    Revisited every thing to find out any errors alongside completing 4th Week task

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,26 +1,19 @@
-/* Random Codes */
-
-//Function to generate combination of characters
 function generateCode() {
-  //Create variables to store generated codes and the type of characters we want to show as codes
-  var code = " "; //initialize to null value
+  var code = "";
   var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
 
-  //Generate character multiple times using a loop
-  for (i = 1; i <= 8; i++) {
-    var char = Math.random() * str.length; //random select a character from the variable and then store in a new variable and then store in a new variable
+  for (var i = 1; i <= 8; i++) {
+    var char = Math.random() * str.length;
     code += str.charAt(char);
   }
-  return code; //return the final accumulated string when loop ends
+
+  return code;
 }
 
-//Get HTML element to display
 document.getElementById("codes").innerHTML = generateCode();
 
-//Disable Button
 function disableButton() {
   document.getElementById("submit").disabled = true;
 }
 
-//Activate Function
 disableButton();
