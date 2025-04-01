d6e1616e2a68e72ada440359cf8b55bd28af89d2
himanshuwestcliff <himanshu.dahal@westcliff.edu>
2025-04-01 02:10:54 -0700

    Tried implementing form changes

--- js/site_scripts.js
+++ js/site_scripts.js
@@ -24,3 +24,20 @@ partners.forEach((partner) => {
   li.appendChild(img);
   partnerList.appendChild(li);
 });
+
+
+<!-- Replace the radios div with this -->
+<div class="selectmenu">
+  <label>How should we contact you?</label>
+  <select id="contactMethod">
+    <option value="select">Select One</option>
+    <option value="email">Email</option>
+    <option value="phone">Phone</option>
+  </select>
+  <div id="emailInput" style="display: none;">
+    <input type="email" name="contactEmail" placeholder="Enter your email" />
+  </div>
+  <div id="phoneInput" style="display: none;">
+    <input type="text" name="contactPhone" placeholder="Enter your phone" />
+  </div>
+</div>
